83bf288e91cc1b15f0d5f2c273de03923be52cc8
laperon <zhehllov@gmail.com>
2016-06-11 12:23:01 +0300

    Добавлена форма регистрации

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/map', function () {
+    return view('map');
+});
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);
