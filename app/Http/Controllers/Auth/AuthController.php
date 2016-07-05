0661d6289cfafe82e9bda5d5785fc37cdd3abefa
laperon <zhehllov@gmail.com>
2016-07-05 10:50:40 +0300

    Bugfix header menu

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/map';
 
     /**
      * Create a new authentication controller instance.
