a53fbe4e4f41150712c2e4990406b939c713c546
antay1551 <antay1551@gmail.com>
2018-05-28 23:34:42 +0300

    structureBase

---
+++ data.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS `travel` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `travel`;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `routes`
+--
+
+CREATE TABLE `routes` (
+  `login` int(10) DEFAULT NULL,
+  `trip` varchar(20) DEFAULT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) DEFAULT NULL,
+  `first_name` varchar(10) DEFAULT NULL,
+  `last_name` varchar(10) DEFAULT NULL,
+  `login` varchar(10) DEFAULT NULL,
+  `password` varchar(10) DEFAULT NULL,
+  `email` varchar(10) DEFAULT NULL,
+  `base_country` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
