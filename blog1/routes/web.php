ebd3a522ba7c6eb4dde53b1c5d08073222b262ca
antay1551 <antay1551@gmail.com>
2018-06-02 12:44:42 +0300

    add login, regist in firstPage. in admin exception

---
+++ blog1/routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('start', function () {
+    return view('site.start');
+});
+
+Route::get('login', function () {
+    return view('site.login');
+});
+
+
+
+
+Route::prefix('admin')->group(function (){
+    Route::get('/login', 'Auth\AdminLoginController@showLoginFrom')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+});
+
+
+Route::get('help', function () {
+    return view('pages.help');
+});
+
+Route::get('about', function () {
+    return view('pages.about');
+});
+
+Route::get('contact', function () {
+    return view('pages.contact');
+});
+
+Route::get('first/second', function () {
+    return view('pages.test');
+});
+Route::get('inform',['uses' => 'UsersController@index']);
+
+Route::get('users/create',['uses' => 'UsersController@create']);
+
+Route::post('users',['uses' => 'UsersController@store']);
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
