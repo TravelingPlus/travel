60478bbb9f5796d60c3ae003ee8de521f6bd9790
Dmitry <dmitryfedoryaka@gmail.com>
2018-06-04 01:54:56 +0300

    account

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+<div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="border:1px solid #000;"><img src="img/icons8-взлет-самолета-80.png" style="width:100%;"></div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align:center;">Plane</div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="border:1px solid #000;"><img src="img/icons8-поезд-80.png" style="width:100%;"></div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align:center;">Train</div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="border:1px solid #000;"><img src="img/icons8-автобус-80.png" style="width:100%;"></div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align:center;">Bus</div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="border:1px solid #000;"><img src="img/icons8-автомобиль-80.png" style="width:100%;"></div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align:center;">Car</div>
+</div>
+<div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="border:1px solid #000;"><img src="img/icons8-кровать-80.png" style="width:100%;"></div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align:center;">Hotel</div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="border:1px solid #000;"><img src="img/icons8-детская-80.png" style="width:100%;"></div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align:center;">Hostel</div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="border:1px solid #000;"><img src="img/icons8-коттедж-80.png" style="width:100%;"></div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align:center;">House</div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="border:1px solid #000;"><img src="img/icons8-трейлер-80.png" style="width:100%;"></div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align:center;">Trailer</div>
+</div>
+<div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
 @endsection
