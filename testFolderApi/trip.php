ce87667e1edc043ddff71dfb917b9433ff7aefc4
Filpizza <faust1234a@gmail.com>
2018-06-15 14:04:16 +0300

    update

--- testFolderApi/trip.php
+++ testFolderApi/trip.php
@@ -6,88 +6,74 @@
  * Time: 23:44
  */
 
-require_once ("index.html");
+require_once("index.html");
 
 
-$id = 1111111;
-
+//Выбор валюты
 if ($_POST['currency'] == 'usd') {
-    $curr = 'usd';
+    $currency = 'usd';
 } elseif ($_POST['currency'] == 'eur') {
-    $curr = 'eur';
+    $currency = 'eur';
 } elseif ($_POST['currency'] == 'rub') {
-    $curr = 'rub';
+    $currency = 'rub';
 } else {
     echo 'Выберите валюту из выше перечисленных';
 }
 
-$depa = $_POST['depart'];
-$retu = $_POST['return'];
+//
+$departureDate = $_POST['departureDate'];
+$ArrivalDate = $_POST['ArrivalDate'];
 
-$from = $_POST['frm'];
-$to = $_POST['too'];
+$departurePoint = $_POST['departurePoint'];
+$arrivalPoint = $_POST['arrivalPoint'];
 
 
 //City codes in format IATA
-$codeCity = file_get_contents('https://www.travelpayouts.com/widgets_suggest_params?q=' . urlencode("Из {$from} в {$to}"));
+$codeCity = file_get_contents('https://www.travelpayouts.com/widgets_suggest_params?q=' . urlencode("Из {$departurePoint} в {$arrivalPoint}"));
 $decodeCity = json_decode($codeCity, true);
 
 
-$gorodOtpravki = $decodeCity['origin']['iata'];
-$gorodPribitiya = $decodeCity['destination']['iata'];
+$cityOfDeparture = $decodeCity['origin']['iata'];
+$cityOfArrival = $decodeCity['destination']['iata'];
 
-$a = $gorodOtpravki;
-$b = $gorodPribitiya;
+$a = $cityOfDeparture;
+$b = $cityOfArrival;
 
 echo '<pre>' . print_r($decodeCity, true) . '</pre>';
 
-
 //Данные о городах в json формате
-$goroda = file_get_contents("http://api.travelpayouts.com/data/cities.json");
-$dannieOgorodax = json_decode($goroda,true);
+$cities = file_get_contents("http://api.travelpayouts.com/data/cities.json");
+$infoOnCities = json_decode($cities, true);
 
-//if ($dannieOgorodax['code'] == $a){
-//}
+// echo '<pre>' . print_r($infoOnCities, true) . '</pre>';
 
-
-
-// echo '<pre>' . print_r($dannieOgorodax, true) . '</pre>';
-
-foreach($dannieOgorodax as $city){
-    if($city['code'] == $a){
-        $lat = $city['coordinates']['lat'];
-        $lon = $city['coordinates']['lon'];
-        echo $lat;
-        echo $lon;
+foreach ($infoOnCities as $city) {
+    if ($city['code'] == $a) {
+        $latitude1 = $city['coordinates']['lat'];
+        $longtude1 = $city['coordinates']['lon'];
     }
 }
 
+foreach ($infoOnCities as $city) {
+    if ($city['code'] == $b) {
+        $latitude2 = $city['coordinates']['lat'];
+        $longtude2 = $city['coordinates']['lon'];
+    }
+}
+
+echo "<script type=\"text/javascript\"> var latitude1=$latitude1;var longtude1=$longtude1;</script>";
+echo "<script type=\"text/javascript\"> var latitude2=$latitude2;var longtude2=$longtude2;</script>";
 
 
 //Departures from and to. According to the calendar and without. Currency
-$tickets = file_get_contents("https://api.travelpayouts.com/v1/prices/calendar?depart_date={$depa}&return_date={$retu}&currency={$curr}&origin={$gorodOtpravki}&destination={$gorodPribitiya}&calendar_type=departure_date&token=ff86a5b4622103a85185456756893056");
-//$tickets = file_get_contents("http://api.travelpayouts.com/v1/prices/calendar?depart_date={$depa}&origin={$a}&destination={$b}&calendar_type=departure_date&token=ff86a5b4622103a85185456756893056");
+$codeTickets = file_get_contents("https://api.travelpayouts.com/v1/prices/calendar?depart_date={$departureDate}&return_date={$ArrivalDate}&currency={$currency}&origin={$cityOfDeparture}&destination={$cityOfArrival}&calendar_type=departure_date&token=ff86a5b4622103a85185456756893056");
+//$codeTickets = file_get_contents("http://api.travelpayouts.com/v1/prices/calendar?depart_date={$departureDate}&origin={$a}&destination={$b}&calendar_type=departure_date&token=ff86a5b4622103a85185456756893056");
 
-$travel = json_decode($tickets, true);
+$decodeTickets = json_decode($codeTickets, true);
 
-//echo '<pre>' . print_r($travel, true) . '</pre>';
+//echo '<pre>' . print_r($decodeTickets, true) . '</pre>';
 //echo '<pre>' . print_r($decodeCity, true) . '</pre>';
 
 
-
-
-//Will print only the prices
-foreach ($dannieOgorodax as $i) {
-    $xex[] = $i['code'] . "<br/>";
-}
-
-
-foreach($xex as $xe) {
-    if ($xe == $a) {
-        foreach ($dannieOgorodax as $x) {
-            echo $x['coordinates']['lat'] . "<br>";
-        }
-    }
-}
 
 
