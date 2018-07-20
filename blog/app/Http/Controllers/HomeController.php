ac38e07648a18ea55cb57b092c0d9cb86b271fcf
antay1551 <antay1551@gmail.com>
2018-07-20 11:57:47 +0300

    add cash trips into db

--- blog/app/Http/Controllers/HomeController.php
+++ blog/app/Http/Controllers/HomeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\dateTrips;
 use App\Saave;
 use Illuminate\Http\Request;
 use App\Trips;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 //use App\Save as ExemplarOfModel;
 class HomeController extends Controller
@@ -33,7 +35,6 @@ class HomeController extends Controller
     public function prepareToSave(Request $request)
     {
 
-       // $add = new ExemplarOfModel();
         $add = new Saave();
         $add->airline= $request['airline'];
         $add->price= $request['price'];
@@ -50,86 +51,40 @@ class HomeController extends Controller
 
         $add->save();
         return ($add);
-        //$res=(json_decode($request, true));
-        //return $request['aaaaa'];
-        //return $request;
     }
 
     public function json(Request $request)
     {
 
         $exemlarTrips = new Trips();
-        $resultTrips = $exemlarTrips->getInformationApi($request);
-        return $resultTrips;
-        //return $exemlarTrips;
-        /*
-         *
-        $currency = $request->input('currency');
+        $exemlarDateTrips = new dateTrips();
 
-        if ($currency == 'usd') {
-            $curr = 'usd';
-        } elseif ($currency == 'eur') {
-            $curr = 'eur';
-        } elseif ($currency == 'rub') {
-            $curr = 'rub';
-        } else {
-            echo 'Выберите валюту из выше перечисленных';
+        $origin = $request['name'][0];
+        $destination = $request['name'][1];
+        $departure_at = $request['depart'];
+        $return_at = $request['return'];
+
+        $resultWhere = DB::table('info_trip')->where('from_city', $origin)->where('to_city', $destination)->where('date_from', $departure_at)->where('date_to', $return_at)->first();
+
+        $flag=0;
+        if (isset($resultWhere)){
+            $flag=1;
+        }else $flag=0;
+
+        if ($flag==0)
+        {
+            $resultTrips = $exemlarTrips->getInformationApi($request);
+            $exemlarDateTrips->to_city = $destination;
+            $exemlarDateTrips->from_city = $origin;
+            $exemlarDateTrips->date_from = $departure_at;
+            $exemlarDateTrips->date_to = $return_at;
+
+            $exemlarDateTrips->save();
+
         }
 
-        $departureDate = $request->input('depart');
-        $ArrivalDate = $request->input('return');
-
-        //$rrr = count($request);
-        $count = 0;
-        $i = 0;
-        while (isset($request->input('name')[$i])) {
-            $count++;
-            $i++;
-        }
-        $departurePoint = $request->input('name')[$count - 2];
-        $arrivalPoint = $request->input('name')[$count - 1];
-
-
-        //City codes in format IATA
-        $codeCity = file_get_contents('https://www.travelpayouts.com/widgets_suggest_params?q=' . urlencode("Из {$departurePoint} в {$arrivalPoint}"));
-        $decodeCity = json_decode($codeCity, true);
-
-
-        $cityOfDeparture = $decodeCity['origin']['iata'];
-        $cityOfArrival = $decodeCity['destination']['iata'];
-
-        $a = $cityOfDeparture;
-        $b = $cityOfArrival;
-
-        //Данные о городах в json формате
-        $cities = file_get_contents("http://api.travelpayouts.com/data/cities.json");
-        $infoOnCities = json_decode($cities, true);
-        $latitude1 = null;
-        $longtude1 = null;
-        $latitude2 = null;
-        $longtude2 = null;
-        foreach ($infoOnCities as $city) {
-            if ($city['code'] == $a) {
-                $latitude1 = $city['coordinates']['lat'];
-                $longtude1 = $city['coordinates']['lon'];
-            }
-        }
-
-        foreach ($infoOnCities as $city) {
-            if ($city['code'] == $b) {
-                $latitude2 = $city['coordinates']['lat'];
-                $longtude2 = $city['coordinates']['lon'];
-            }
-        }
-
-        //Departures from and to. According to the calendar and without. Currency
-        $codeTickets = file_get_contents("https://api.travelpayouts.com/v1/prices/calendar?depart_date={$departureDate}&return_date={$ArrivalDate}&currency={$currency}&origin={$cityOfDeparture}&destination={$cityOfArrival}&calendar_type=departure_date&token=ff86a5b4622103a85185456756893056");
-        $info = [0 => $latitude1, 1 => $longtude1, 2 => $latitude2, 3 => $longtude2];
-        $res = [0 => $codeTickets, 1 => $info, 2 => $departureDate];
-        return $res;
-        //return $count;
-        */
-
+        //return $resultTrips;
+        return $flag;
     }
 
     public function popular(Request $request)
